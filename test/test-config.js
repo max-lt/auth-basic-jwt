93d85a8b2effb24689de8c56ce03bde5fd3ecf66
maxx-t <maxime.tissier@gmail.com>
2017-03-02 19:51:35 +0100

    added test

--- test/test-config.js
+++ test/test-config.js
@@ -44,3 +44,34 @@ describe('test secret', () => {
     });
 
 });
+
+
+describe('test userGetter', () => {
+
+    it('should fail if not set', () => {
+        assert.throws(() => auth('secret', null), Error)
+    });
+
+    it('should fail on use if set with empty promise', (done) => {
+        const _auth = auth('secret', Promise.resolve());
+        const {ERROR_500_RESPONSE, app, btoa, hasUserMatchingUser, hasToken} = common(_auth);
+        request(app)
+            .post('/login')
+            .set('Authorization', 'Basic ' + btoa('user:pass'))
+            .expect(500)
+            .expect((res) => res.body == ERROR_500_RESPONSE)
+            .end(done);
+    });
+
+    it('should fail on use if set with invalid promise return', (done) => {
+        const _auth = auth('secret', Promise.resolve(142));
+        const {ERROR_500_RESPONSE, app, btoa, hasUserMatchingUser, hasToken} = common(_auth);
+        request(app)
+            .post('/login')
+            .set('Authorization', 'Basic ' + btoa('user:pass'))
+            .expect(500)
+            .expect((res) => res.body == ERROR_500_RESPONSE)
+            .end(done);
+    });
+
+});
